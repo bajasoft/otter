8e21f84bc0d739e232bbf6b0fdbdf36db4d007a3
bajasoft <jan.bajer@seznam.cz>
2016-08-05 11:24:35 +0200

    Initial extending patch for PS script

--- packaging/otter-browser.ps1
+++ packaging/otter-browser.ps1
@@ -23,7 +23,10 @@ $Global:inputPath = "C:\downloads\Otter\"
 $Global:7ZipBinaryPath = "C:\Program Files\7-Zip\7z.exe"
 $Global:innoBinaryPath = "C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
 $Global:innoScriptPath = ".\otter-browser.iss"
+$Global:updateScriptPath = ".\otter-browser.rb"
+$Global:rubyPath = "C:\Ruby22-x64\bin\ruby.exe"
 $Global:configPath = ".\otter\config.h"
+$Global:updateConfiguration = ".\otter-browser-update-win.json"
 
 # Main-function
 function main 
@@ -106,17 +109,36 @@ function main
      } |
     Set-Content $Global:innoScriptPath
 
+    # Set package name to updater config
+    $content = Get-Content $Global:updateConfiguration
+
+    $content |
+    ForEach-Object {
+        $_ -replace ".*? `: \[$", "`t`t`"$packageName`" `: ["
+    } |
+    Set-Content $Global:updateConfiguration
+
     # Do full build
-    fullBuild $7zipFileName $zipFileName
+    fullBuild $7zipFileName $mainVersion $zipFileName
+
+    $updateXml = ($packageName + ".xml");
+
+    If (Test-Path $updateXml)
+    {
+        Remove-Item $updateXml
+    }
+
+    # Rename update script with current version/platform
+    Rename-Item ($Global:outputPath + "/otter-browser-update.xml") $updateXml
 
     "Finished!"
 }
 
-function fullBuild ($7zipFileName, $zipFileName)
+function fullBuild ($7zipFileName, $mainVersion, $zipFileName)
 {
     Start-Process -NoNewWindow -Wait $Global:innoBinaryPath -ArgumentList $Global:innoScriptPath
     Start-Process -NoNewWindow -Wait $Global:7ZipBinaryPath -ArgumentList "a", $7zipFileName, ($Global:inputPath + "*"), "-mmt4", "-mx9", "-m0=lzma2"
-    Start-Process -NoNewWindow -Wait $Global:7ZipBinaryPath -ArgumentList "a", "-tzip", $zipFileName, $Global:inputPath, "-mx5"
+    Start-Process -NoNewWindow -Wait $Global:rubyPath -ArgumentList $Global:updateScriptPath, $Global:inputPath, $Global:updateConfiguration, $Global:outputPath, "-p", "windows", "-v", $mainVersion
 }
 
 # Entry point
