fcbd3551048da04fecee4d38fc91c599fd820b8d
bajasoft <jan.bajer@seznam.cz>
2017-10-29 13:55:07 +0100

    Show correct latest version in update notification

--- src/core/Application.h
+++ src/core/Application.h
@@ -1,7 +1,7 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
 * Copyright (C) 2013 - 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
-* Copyright (C) 2015 - 2016 Jan Bajer aka bajasoft <jbajer@gmail.com>
+* Copyright (C) 2015 - 2017 Jan Bajer aka bajasoft <jbajer@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -97,7 +97,7 @@ protected slots:
 	void handleAboutToQuit();
 	void handleNewConnection();
 	void handleFocusObjectChanged(QObject *object);
-	void handleUpdateCheckResult(const QVector<UpdateChecker::UpdateInformation> &availableUpdates);
+	void handleUpdateCheckResult(const QVector<UpdateChecker::UpdateInformation> &availableUpdates, int latestVersionIndex);
 	void showUpdateDetails();
 
 private:
